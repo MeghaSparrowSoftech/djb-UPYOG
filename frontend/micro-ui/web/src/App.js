864556bef71b45b3f46656fa3ba6cee543fff929
ArshSrivastava21-dot <arsh_srivastava@sparrowsoftech.net>
2026-01-28 14:26:55 +0530

    fix: ensure contextPath is set in App component

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -111,9 +111,11 @@ const moduleReducers = (initData) => ({
 });
 
 function App() {
+    window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH") || "digit-ui";
   const stateCode =
     window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") ||
     process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
+
   if (!stateCode) {
     return <h1>stateCode is not defined</h1>;
   }
